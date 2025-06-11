2561e18cfffbff650c22c1c56af47335dfb6262e
christian calderon <print.christiancalderon@gmail.com>
2025-06-10 22:22:26 -0700

    done

--- db/seed.js
+++ db/seed.js
@@ -5,6 +5,21 @@ await seedEmployees();
 await db.end();
 console.log("🌱 Database seeded.");
 
+import db from "#db/client";
+import { createEmployee } from "#db/queries/employees";
+
+await db.connect();
+await seedEmployees();
+await db.end();
+console.log("🌱 Database seeded.");
+
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 15; i++) {
+    const employee = {
+      name: "Employee " + i,
+      birthday: "1001-10-01",
+      salary: Math.floor(Math.random() * 500000),
+    };
+    await createEmployee(employee);
+  }
 }
